b51369233d8bc8bb95bdd62f1b110ff0f503a17d
Adam Parrish <aparrish@neosavvy.com>
2017-01-25 17:42:32 -0500

    Initial Commit
    ## Change Summary
    
    Time boxed exercise for calculating a percentage breakdown for Deadlift / Squat / Bench
    
    ## TESTS
    
    Not yet - better learn this next.

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,187 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    TextInput,
+    Text,
+    StyleSheet,
+    Button,
+    View
 } from 'react-native';
 
+const PERCENTAGES = [
+    95,
+    90,
+    85,
+    80,
+    75,
+    70,
+    65,
+    60,
+    55,
+    50,
+    45,
+    40,
+    35,
+    30,
+    25,
+    20,
+    15,
+    10,
+    5
+];
+
+const initialState = {
+    isComputed: false,
+    maxBench: null,
+    maxSquat: null,
+    maxDeadLift: null,
+    benchPercentages: [],
+    squatPercentages: [],
+    deadLiftPercentages: [],
+    activePercentages: []
+};
+
 export default class LiftCalculator extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = initialState;
+    }
+
+    onShow = (activePercentages) => {
+        this.setState({
+            activePercentages
+        })
+    };
+
+    onReset = () => {
+        this.setState(initialState)
+    };
+
+    computePercentagesOf(maxValue) {
+        return _.reduce(PERCENTAGES, (acc, percentage) => {
+            const computation = {
+                percentageOfMax: percentage,
+                value: (percentage / 100) * maxValue
+            };
+            return acc.concat(computation);
+        }, [])
+    }
+
+    onCalculate = () => {
+        const {
+            maxBench,
+            maxSquat,
+            maxDeadLift
+        } = this.state;
+
+        const benchPercentages = this.computePercentagesOf(maxBench);
+        const squatPercentages = this.computePercentagesOf(maxSquat);
+        const deadLiftPercentages = this.computePercentagesOf(maxDeadLift);
+
+        this.setState({
+            benchPercentages,
+            squatPercentages,
+            deadLiftPercentages,
+            isComputed: true
+        });
+    };
+
+    render() {
+        if(this.state.isComputed) {
+            return (
+                <View style={styles.container}>
+
+                    {
+                        _.map(this.state.activePercentages, (x) => {
+                            return (
+                                <Text style={styles.text}>
+                                    {x.percentageOfMax}: {x.value}
+                                </Text>
+                            )
+                        })
+                    }
+
+                    <Button
+                        style={styles.button}
+                        onPress={() => {this.onShow(this.state.deadLiftPercentages)}}
+                        title="Deadlift"
+                        color="#841584"
+                    />
+                    <Button
+                        style={styles.button}
+                        onPress={() => {this.onShow(this.state.squatPercentages)}}
+                        title="Squats"
+                        color="#841584"
+                    />
+                    <Button
+                        style={styles.button}
+                        onPress={() => {this.onShow(this.state.benchPercentages)}}
+                        title="Bench"
+                        color="#841584"
+                    />
+                    <Button
+                        style={styles.button}
+                        onPress={this.onReset}
+                        title="Reset"
+                        color="#841584"
+                    />
+                </View>
+            );
+        } else {
+            return (
+                <View style={styles.container}>
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Max Bench Press (in lbs)"
+                        onChangeText={(maxBench) => this.setState({maxBench})}
+                    />
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Max Squat (in lbs)"
+                        onChangeText={(maxSquat) => this.setState({maxSquat})}
+                    />
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Max Dead Lift (in lbs)"
+                        onChangeText={(maxDeadLift) => this.setState({maxDeadLift})}
+                    />
+                    <Button
+                        style={styles.button}
+                        onPress={this.onCalculate}
+                        title="Calculate Lift Percentages"
+                        color="#841584"
+                    />
+                </View>
+            );
+        }
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    button: {
+        width: 85,
+        height: 45
+    },
+    textInput: {
+        width: 225,
+        height: 45
+    },
+    text: {
+        height: 25,
+        width: 190
+    }
 });
 
 AppRegistry.registerComponent('LiftCalculator', () => LiftCalculator);
