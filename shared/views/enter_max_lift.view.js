11736bb2c8377155dc06d87c9e0f94dbb5b61481
Adam Parrish <aparrish@neosavvy.com>
2017-01-29 19:39:45 -0500

    React Navigator
    ### CHANGE SUMMARY
    
    Adding a react navigator to switch views.

--- shared/views/enter_max_lift.view.js
+++ shared/views/enter_max_lift.view.js
@@ -46,6 +46,7 @@ class EnterMaxLiftView extends Component {
                     onPress={() => {
                         this.props.calculatePercentages();
                         navigate('ShowPercentagesForLiftView');
+
                     }}
                     title="Calculate Lift Percentages"
                     color="#841584"
